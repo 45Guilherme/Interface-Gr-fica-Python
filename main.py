f348fe2e09ec2638308ad933e3be1d4cfa0d4676
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-12-19 13:57:03 -0300

    sexto commit

--- main.py
+++ main.py
@@ -3,28 +3,26 @@ import customtkinter as ctk
 
 app = ctk.CTk()
 
-ctkFrame = ctk.CTkFrame(master=app)
-ctkFrame.pack(pady=20, padx=60, fill="both", expand=True)
-
-
-
 app.title("MiNI SIEM")
 texto_label = "Teste do Mini SIEM"
 app.geometry("800x500")
 
-label_boa_vindas = ctk.CTkLabel(master=app, text=texto_label, font=("Arial", 24))
+label_boa_vindas = ctk.CTkLabel(
+    master=app,
+    text="Teste do Mini SIEM",
+    font=("Arial", 24)
+)
 label_boa_vindas.pack(pady=20)
+
+frame = ctk.CTkFrame(master=app)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
 
 def analisar_logs():
     label_boa_vindas.configure(text="Status: Analisando logs...")
 
 
-ctkFrameButton = ctk.CTkButton(master=ctkFrame, text="Selecionar Arquivo de Logs")
-ctkFrameButton.pack(pady=10, padx=10)
-
-
-
-
+Botao = ctk.CTkButton(master=frame, text="Selecionar Arquivo de Logs", command=analisar_logs)
+Botao.pack(pady=10)
 
 
 app.mainloop()
