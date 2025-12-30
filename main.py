98e3079a247d4b6189d6f1de60aa8bd53d56acec
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-12-30 12:47:16 -0300

    nono commit

--- main.py
+++ main.py
@@ -20,8 +20,18 @@ frame.pack(pady=20, padx=60, fill="both", expand=True)
 frame_usuario_ssh = ctk.CTkEntry(master=frame, width=250, placeholder_text="Usuário SSH")
 frame_usuario_ssh.pack(pady=10)
 
+label_ssh = ctk.CTkLabel(master=frame, text=f"Status: aguardando ação")
+label_ssh.pack(pady=10)
+
 def analisar_logs():
     label_boa_vindas.configure(text="Status: Analisando logs...")
+    ssh = frame_usuario_ssh.get()
+    label_ssh.configure(text=f"Status: Analisando logs para o usuário SSH: {ssh}")
+    if ssh == "":
+        print("Status: usuário SSH não informado")
+    else:
+        print(f"Status: Analisando logs para o usuário SSH: {ssh}")
+
 
 
 Botao = ctk.CTkButton(master=frame, text="Selecionar Arquivo de Logs", command=analisar_logs)
